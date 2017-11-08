9181a7dd9f8af090a08d1d0545e0df978e65b292
jengleberg <jeffengleberg@hotmail.com>
2017-11-08 08:54:29 -0700

    completes sprint 3

--- starter-code/back-end/db/seed.js
+++ starter-code/back-end/db/seed.js
@@ -1,13 +1,49 @@
 var DB = require("../models").models;
 
- var artistCreate = function() {
+var lucySongs = [
+     {
+          title: "O sole mio",
+          duration: "3:21",
+          date_of_release: "1990",
+          album_title: "Three Tenors in Concert",
+          artistId: ""
+     },
+     {
+          title: "Nessun dorma",
+          duration: "3:21",
+          date_of_release: "1990",
+          album_title: "Three Tenors in Concert",
+          artistId: ""
+     }
+];
+
+var rickyAd = 
+     [ {
+          headline: "The Best Ad Ever",
+          url: "https://upload.wikimedia.org/wikipedia/commons/2/2a/New_Logo_AD.jpg",
+          managerId: ""
+     }
+     ];
+
+
+
+ var artistCreate = function(manager) {
  	return DB.Artist.create({
      name: 'Luciano Pavarotti',
      photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
      nationality: 'Italiano',
      instrument: 'Voice',
-     home_address: '1 Strada Roma'
-   });
+     home_address: '1 Strada Roma',
+     managerId: manager.id
+   })
+     .then(function(artist) {
+     lucySongs.forEach(function(song) {
+          song.artistId = artist.id;
+     });
+     DB.Song.bulkCreate(lucySongs);
+  });
+     
+
  };
  
  var managerCreate = function() {
@@ -15,9 +51,21 @@ var DB = require("../models").models;
      name: 'Ricky Bobby',
      email: 'rbobby@gmail.com',
      office_number: '516-877-0304',
-     cell_phone_number: '718-989-1231'
- 	});
- };
+     cell_phone_number: '718-989-1231',
+   })
+     .then(function(manager) {
+          rickyAd.forEach(function(ad) {
+               ad.managerId = manager.id;
+               console.log(manager.id);
+          });
+          console.log(rickyAd);
+          DB.Ad.bulkCreate(rickyAd);
+          artistCreate(manager); 
+     });
+     
+     
+ 
+};
  
  var songCreate = function() {
  	return DB.Song.create({
@@ -27,9 +75,17 @@ var DB = require("../models").models;
  	    album_title: 'Best Album Ever'
  	});
  };
+
+//  var adCreate = function() {
+//      return DB.Ad.create({
+//           headline: "",
+//           url: ""
+            
+//      });
+
+// };
  
- artistCreate()
- .then(managerCreate)
+  managerCreate()
  .then(songCreate)
  .then(function() {
  	process.exit();
